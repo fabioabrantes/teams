dd1a8f10f3738ad34f940bd91fef031fcc2f8dd2
fabioabrantes <fabio.abrantes.diniz@gmail.com>
2024-07-02 10:28:39 -0300

    groups finished

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,9 +1,75 @@
+import { useCallback, useState } from 'react';
+import { FlatList } from 'react-native';
+import { useFocusEffect, useNavigation } from '@react-navigation/native';
+
+import { Header } from '@components/Header';
+import { Highlight } from '@components/Highlight';
+import { GroupCard } from '@components/GroupCard';
+import { ListEmpty } from '@components/ListEmpty';
+
 import { Container } from './styles';
+import { Button } from '@components/Button';
+
+import { groupsGetAll } from '@storage/group/groupsGetAll';
 
 export function Groups() {
+  const [groups, setGroups] = useState<string[]>([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  const navigation = useNavigation()
+
+  function handleNewGroup() {
+    navigation.navigate('newGroup');
+  }
+  
+  function handleOpenGroup(group: string) {
+    navigation.navigate('players', { group })
+  }
+
+  async function fetchGroups() {
+    try {
+      setIsLoading(true);
+      const data = await groupsGetAll();
+      setGroups(data)
+    } catch (error) {
+      console.log(error);
+    }finally{
+      setIsLoading(false);
+    }
+  }
+
+  useFocusEffect(useCallback(() => {
+    fetchGroups()
+  },[]));
+
   return (
     <Container>
+      <Header />
 
+      <Highlight
+        title="Turmas"
+        subtitle="jogue com a sua turma"
+      />
+
+      <FlatList
+        data={groups}
+        keyExtractor={item => item}
+        renderItem={({ item }) => (
+          <GroupCard
+            title={item}
+            onPress={() => handleOpenGroup(item)}
+          />
+        )}
+        contentContainerStyle={groups.length === 0 && { flex: 1 }}
+        ListEmptyComponent={() => (
+          <ListEmpty message="Que tal cadastrar a primeira turma?" />
+        )}
+      />
+
+      <Button
+        title='Criar nova turma'
+        onPress={handleNewGroup}
+      />
     </Container>
   );
 }
